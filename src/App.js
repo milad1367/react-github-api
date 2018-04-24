b402e1ac710d4d3560486cd47eb6f9920aa79151
milad asghari <miladasghari2016@gmail.com>
2018-04-24 13:08:46 +0430

    get issues list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import IssuesList from './components/IssuesList';
 class App extends Component {
--- src/components/IssuesList.js
+++ src/components/IssuesList.js
@@ -2,7 +2,18 @@ import React, { Component } from 'react';
 import Axios from 'axios';
 
 class IssuesList extends Component {
-
+    constructor(props) {
+        super(props);
+        this.state = { issues : [] }
+    }
+    componentDidMount() {
+        const GithubOpenIssuesUrl = "https://api.github.com/repos/angular/angular/issues?state=open";
+        Axios.get(GithubOpenIssuesUrl)
+         .then(res=> {
+             this.setState({ issues : res.data });
+         })
+         .then(() => console.log(this.state.issues));
+    }
     render() {
         return (
             <div>
