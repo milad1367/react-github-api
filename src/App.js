e34d74aa3199e2f15e7ac0dfdc8111af803d8d88
milad asghari <miladasghari2016@gmail.com>
2018-04-24 12:35:26 +0430

    create IssuesList

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import IssuesList from './components/IssuesList';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">React issues list : </h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <IssuesList />
       </div>
     );
   }
---
+++ src/components/IssuesList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class IssuesList extends Component {
+    render() {
+        return (
+            <div>
+               <ul> 
+                   <li> one </li>
+               </ul>
+            </div>
+        )
+    }
+}
+
+export default IssuesList
