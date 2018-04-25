8246836b04c5d0c1afab1470c65a469e883d7777
milad asghari <miladasghari2016@gmail.com>
2018-04-25 12:59:28 +0430

    add list issues and markdown to jsx

--- src/components/IssuesList.js
+++ src/components/IssuesList.js
@@ -1,26 +1,37 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
-
+import Markdown from 'markdown-to-jsx';
 class IssuesList extends Component {
     constructor(props) {
         super(props);
         this.state = { issues : [] }
     }
     componentDidMount() {
-        const GithubOpenIssuesUrl = "https://api.github.com/repos/angular/angular/issues?state=open";
+        const GithubOpenIssuesUrl = "https://api.github.com/repos/facebook/react/issues?state=open";
         Axios.get(GithubOpenIssuesUrl)
          .then(res=> {
              this.setState({ issues : res.data });
          })
-         .then(() => console.log(this.state.issues));
+         .then(() => { console.log(this.state.issues)});
     }
     render() {
+
+        let titles;
+        if (this.state.issues.length > 1 ) {
+             titles = this.state.issues.map((item) => 
+                 <li key = {item.id.toString()}>
+                      {<Markdown>{item.title}</Markdown>} 
+                 </li>         
+            )          
+        }
+        else {
+             titles = "loading ... ";
+
+        }
         return (
-            <div>
-               <ul> 
-                   <li> one </li>
-               </ul>
-            </div>
+            <ul>
+                {titles}
+            </ul>
         )
     }
 }
